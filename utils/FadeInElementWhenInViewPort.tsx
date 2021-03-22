a77a2a0a60d71f00ba9e2e43212fc3f20ad66f3b
Jackson <jacksonmain9@gmail.com>
2021-03-22 14:18:26 -0500

    duration 300 seems to be the only one that works

--- utils/FadeInElementWhenInViewPort.tsx
+++ utils/FadeInElementWhenInViewPort.tsx
@@ -45,7 +45,7 @@ export const FadeInElementWhenInViewPort: FC<ToastProps> = ({
   }, [isVisible]);
 
   const rootClassName = cn(
-    "transition-all ease-in-out duration-500",
+    "transition-all ease-in-out duration-300",
     {
       transform: true,
       "opacity-100": isVisible,
