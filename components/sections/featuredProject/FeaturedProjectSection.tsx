cf781375309811cae0c9d46507af4e0e875ce678
Jackson <jacksonmain9@gmail.com>
2021-03-23 18:03:57 -0500

    added iphone mockup for featured Project

--- components/sections/featuredProject/FeaturedProjectSection.tsx
+++ components/sections/featuredProject/FeaturedProjectSection.tsx
@@ -11,7 +11,7 @@ import {
 
 const FeaturedProjectSection: FC = () => {
   return (
-    <div className="relative">
+    <div className="relative bg-gradient-to-br from-white to-gray-300">
       <div className="absolute top-0 mx-auto">
         <TopWaveSolid className="w-screen max-h-40 -mt-1" />
       </div>
@@ -28,83 +28,117 @@ const FeaturedProjectSection: FC = () => {
 
       <div className="max-w-screen-xl mx-auto">
         <section id="ftProjSection">
-          <div className="mx-4 md:mx-auto pt-10 pb-28 md:pt-12 sm:pb-40 md:pb-48 xl:pb-72 xl:pt-24 grid grid-cols-1 border-l border-r border-gray-200 md:border-none">
-            <div className="md:mt-32 mt-14">
-              <div className="flex justify-center">
-                <FtProjIcon className="h-20 w-20 lg:h-28 lg:w-28" />
-              </div>
-              <h2 className="text-primaryDark uppercase font-bold tracking-widest text-2xl md:text-3xl lg:text-4xl mt-4 lg:mt-6 text-center">
-                Featured Project
-              </h2>
+          <div className="mx-4 pt-10 pb-28 md:pt-12 sm:pb-40 md:pb-48 xl:pb-96 xl:pt-32 grid grid-cols-12 border-l border-r border-gray-200 md:border-none">
+            <div className="hidden md:block col-span-8 lg:col-span-7 mt-28 lg:mt-16 -ml-16 lg:-ml-24">
+              <img src="./ftProjMockup2.svg" className="" alt="" />
+            </div>
 
-              <div className="text-center mt-5 lg:mt-8">
-                <h3 className="text-xl font-semibold text-gray-900 md:text-2xl lg:text-4xl">
-                  KollabMe
-                </h3>
-                <div className="mt-2">
-                  <Link href="https://kollab-me.herokuapp.com/community">
-                    <a className="text-gray-600 md:text-xl focus:outline-none focus:ring-2 focus:ring-purple-400 rounded-md px-2 py-1 hover:underline">
-                      kollabme.app
-                    </a>
-                  </Link>
+            <div className="col-span-12 md:col-span-4 lg:col-span-5 lg:pr-10 md:-ml-28">
+              <div className="md:mt-24 mt-14 px-4 lg:px-0">
+                <div className="flex items-center justify-center">
+                  <FtProjIcon className="h-20 w-20 lg:h-24 lg:w-24 lg:mt-6 ml-6 mt-5" />
+                </div>
+                <h2 className="text-primaryDark uppercase font-bold tracking-widest text-2xl md:text-3xl lg:text-4xl mt-4 lg:mt-6 text-center">
+                  Featured Project
+                </h2>
+
+                <div className="text-center md:text-center mt-5">
+                  <h3 className="text-xl font-semibold text-gray-900 md:text-2xl lg:text-3xl">
+                    KollabMe
+                  </h3>
+                  <div className="lg:mt-2">
+                    <Link href="https://kollab-me.herokuapp.com/community">
+                      <a className="text-gray-600 md:text-xl focus:outline-none focus:ring-2 focus:ring-purple-400 rounded-md py-1 hover:underline">
+                        kollabme.app
+                      </a>
+                    </Link>
+                  </div>
                 </div>
               </div>
+
+              <div className="mx-auto md:mx-0 mt-10 lg:text-lg">
+                <span className="block text-gray-800 font-semibold border-l-4 -ml-1 border-primaryDark md:border-none pl-4 lg:pl-4 md:-ml-5 md:pl-8">
+                  What?
+                </span>
+
+                <p className="text-gray-600 px-4 lg:px-0 mt-2 md:leading-7 xl:leading-9 max-w-2xl">
+                  An online platform to <em className="text-primaryDark">develop</em>,{" "}
+                  <em className="text-primaryDark">connect</em> and{" "}
+                  <em className="text-primaryDark">discover</em> individuals from all avenues
+                  of entertainment, such as artists, musicians, audio engineers, comedians and
+                  painters.
+                </p>
+              </div>
+
+              <div className="md:hidden mx-4 mt-16 md:mt-24 lg:mt-16 w-full sm:mx-auto justify-center flex items-center sm:space-x-14">
+                <img
+                  className="mx-auto ml-7 rounded-lg object-center object-cover"
+                  src="./ftProjMockup2.svg"
+                  alt=""
+                />
+              </div>
+
+              <div className="lg:mt-16 mt-20 mx-auto lg:text-lg">
+                <span className="block text-gray-800 font-semibold border-l-4 -ml-1 border-primaryDark md:border-none px-4 lg:pl-5 md:-ml-6 md:pl-8">
+                  Why?
+                </span>
+
+                <p className="text-gray-600 px-4 md:px-0 mt-2 md:leading-7 xl:leading-9 max-w-2xl">
+                  It's difficult for amateur artists, musicians, comedians and painters to
+                  recieve recognition and promote their original works. This platform provides
+                  a way for those individuals to <em className="text-primaryDark">showcase</em>{" "}
+                  and promote their craft as well as connect with others in adjacent fields.{" "}
+                </p>
+              </div>
+              <div className="pb-10">
+                <Link href="https://kollab-me.herokuapp.com/community">
+                  <a className="py-2 flex text-white font-semibold tracking-wide text-sm bg-primaryDark justify-center items-center mt-24 w-32  space-x-1 mx-auto md:mx-0 lg:mt-24 rounded-lg shadow-md hover:bg-opacity-80 focus:outline-none focus:ring-2 focus:ring-purple-400 transition duration-200 ease-in-out cursor-pointer">
+                    Learn more
+                    <ArrowRight className="h-5 w-5 text-white" />
+                  </a>
+                </Link>
+              </div>
             </div>
 
-            <div className="mx-auto mt-20 lg:text-lg">
-              <span className="block text-gray-800 font-semibold border-l-4 -ml-1 border-primaryDark md:border-none pl-4 md:-ml-6 md:pl-8">
-                What?
-              </span>
+            {/* <div className="px-4 mt-20 mx-auto w-7/12">
+              <p className="text-gray-600">Technologies Used:</p>
+              <div className="flex flex-wrap items-center mx-auto w-full max-w-72">
+                <span className="block bg-purple-100 px-3 mr-2 mt-2 text-sm md:text-base py-1 text-purple-700 rounded-full font-medium">
+                  ReactJS
+                </span>
+                <span className="block bg-purple-100 px-3 mr-2 mt-2 text-sm md:text-base py-1 text-purple-700 rounded-full font-medium">
+                  JavaScript
+                </span>
+                <span className="block bg-purple-100 px-3 mr-2 mt-2 text-sm md:text-base py-1 text-purple-700 rounded-full font-medium">
+                  GraphQL
+                </span>
+                <span className="block bg-purple-100 px-3 mr-2 mt-2 text-sm md:text-base py-1 text-purple-700 rounded-full font-medium">
+                  Webpack
+                </span>
+                <span className="block bg-purple-100 px-3 mr-2 mt-2 text-sm md:text-base py-1 text-purple-700 rounded-full font-medium">
+                  TailwindCSS
+                </span>
 
-              <p className="text-gray-600 px-4 mt-2 md:leading-7 xl:leading-9 max-w-2xl mx-auto">
-                An online platform to <em className="text-primaryDark">develop</em>,{" "}
-                <em className="text-primaryDark">connect</em> and{" "}
-                <em className="text-primaryDark">discover</em> individuals from all avenues of
-                entertainment, such as artists, musicians, audio engineers, comedians and
-                painters.
-              </p>
-            </div>
-
-            <div className="mx-4 mt-16 md:mt-24 lg:mt-16 sm:mx-auto justify-center flex items-center sm:space-x-14">
-              <button
-                aria-label="previous featured project image"
-                className="shadow-md rounded-md px-1 hidden sm:block focus:outline-none focus:ring-2 focus:ring-purple-400 transition duration-300 ease-in-out"
-              >
-                <ArrowLeft className="h-8 w-8 text-gray-500" />
-              </button>
-              <img
-                className="mx-auto max-w-xs w-full max-w-72 sm:w-72 md:w-96 h-48 md:h-60 lg:h-56  rounded-lg object-center object-cover shadow-lg"
-                src="https://images.unsplash.com/photo-1543069190-9d380c458bc2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"
-                alt=""
-              />
-              <button
-                aria-label="next featured project image"
-                className="shadow-md rounded-md px-1 ml-8 hidden sm:block focus:outline-none focus:ring-2 focus:ring-purple-400 transition duration-300 ease-in-out"
-              >
-                <ArrowRight className="h-8 w-8 text-gray-500 " />
-              </button>
-            </div>
-
-            <div className="lg:mt-28 mt-20 mx-auto lg:text-lg">
-              <span className="block text-gray-800 font-semibold border-l-4 -ml-1 border-primaryDark md:border-none px-4 md:-ml-6 md:pl-8">
-                Why?
-              </span>
-
-              <p className="text-gray-600 px-4 mt-2 md:leading-7 xl:leading-9 max-w-2xl  mx-auto">
-                It's difficult for amateur artists, musicians, comedians and painters to get
-                recognition and promote their original works. This platform provides a way for
-                individuals to showcase and promote their craft as well as connect with others
-                in adjacent fields.{" "}
-              </p>
-            </div>
-            <div className="pb-10">
-              <Link href="https://kollab-me.herokuapp.com/community">
-                <a className="py-2 flex text-white font-semibold tracking-wide text-sm bg-primaryDark justify-center items-center mt-24 w-32  space-x-1 mx-auto lg:mt-36 rounded-lg shadow-md hover:bg-opacity-80 focus:outline-none focus:ring-2 focus:ring-purple-400 transition duration-200 ease-in-out cursor-pointer">
-                  Learn more
-                  <ArrowRight className="h-5 w-5 text-white" />
-                </a>
-              </Link>
-            </div>
+                <span className="block bg-purple-100 px-3 mr-2 mt-2 text-sm md:text-base py-1 text-purple-700 rounded-full font-medium">
+                  NodeJS
+                </span>
+                <span className="block bg-purple-100 px-3 mr-2 mt-2 text-sm md:text-base py-1 text-purple-700 rounded-full font-medium">
+                  MongoDB
+                </span>
+                <span className="block bg-purple-100 px-3 mr-2 mt-2 text-sm md:text-base py-1 text-purple-700 rounded-full font-medium">
+                  Apollo
+                </span>
+                <span className="block bg-purple-100 px-3 mr-2 mt-2 text-sm md:text-base py-1 text-purple-700 rounded-full font-medium">
+                  Redis
+                </span>
+                <span className="block bg-purple-100 px-3 mr-2 mt-2 text-sm md:text-base py-1 text-purple-700 rounded-full font-medium">
+                  AWS
+                </span>
+                <span className="block bg-purple-100 px-3 mr-2 mt-2 text-sm md:text-base py-1 text-purple-700 rounded-full font-medium">
+                  Express
+                </span>
+              </div>
+            </div> */}
           </div>
 
           {/* <div className="absolute top-0 right-0 mt-96 ">
