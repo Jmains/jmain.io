a08729214e216b4a01865f9984f31bba31a44167
Jackson <jacksonmain9@gmail.com>
2021-03-27 14:25:09 -0500

    removed framer motion and added own css animations

--- components/sections/hero/HeroSection.tsx
+++ components/sections/hero/HeroSection.tsx
@@ -2,7 +2,6 @@ import { ArrowDown, ThreeOctagon, Github } from "@components/icons";
 import React, { FC, useEffect, useState } from "react";
 import s from "./HeroSection.module.css";
 import Link from "next/link";
-import { motion } from "framer-motion";
 import { scrollToElement } from "@utils/scrollToElement";
 
 const HeroSection: FC = () => {
@@ -13,81 +12,6 @@ const HeroSection: FC = () => {
   //     setOffSet(windowHeight);
   //   }
   // }, []);
-
-  const imageVariants = {
-    hidden: { opacity: 0, y: -70 },
-    show: {
-      opacity: 1,
-      transition: {
-        duration: 1,
-        delay: 0.2,
-        ease: "easeOut",
-      },
-      y: 0,
-    },
-  };
-
-  const titleVariants = {
-    hidden: { opacity: 0, y: -70 },
-    show: {
-      opacity: 1,
-      transition: {
-        duration: 1,
-        delay: 0.2,
-        ease: "easeOut",
-      },
-      y: 0,
-    },
-  };
-
-  const subTitleVariants = {
-    hidden: { opacity: 0, y: -70 },
-    show: {
-      opacity: 1,
-      transition: {
-        duration: 1,
-        delay: 0.3,
-        ease: "easeOut",
-      },
-      y: 0,
-    },
-  };
-
-  const headlineVariants = {
-    hidden: { opacity: 0 },
-    show: {
-      opacity: 1,
-      transition: {
-        duration: 1,
-        delay: 0.8,
-        ease: "easeIn",
-      },
-    },
-  };
-
-  const callToActionBtnVariants = {
-    hidden: { opacity: 0 },
-    show: {
-      opacity: 1,
-      transition: {
-        duration: 0.5,
-        delay: 1,
-        ease: "easeIn",
-      },
-    },
-  };
-
-  const arrowDownVariants = {
-    hidden: { opacity: 0 },
-    show: {
-      opacity: 1,
-      transition: {
-        duration: 0.5,
-        delay: 1,
-      },
-      y: 0,
-    },
-  };
 
   return (
     <div className="bg-primaryBgDark ">
@@ -103,87 +27,48 @@ const HeroSection: FC = () => {
 
               <div className="sm:px-32 px-4 pt-24 text-center lg:text-left lg:absolute lg:px-14 lg:py-0 lg:top-auto lg:mt-20">
                 {/* Profile Pic small screen */}
-                <motion.div
-                  initial="hidden"
-                  animate="show"
-                  variants={imageVariants}
-                  className="lg:hidden"
-                >
+                <div className="lg:hidden">
                   <img
-                    className=" h-24 w-24 rounded-full object-cover object-center mx-auto shadow-md lg:hidden"
+                    className={s.smScreenProfileImg}
                     src="/headshot.JPG"
                     alt="picture of me"
                   />
-                </motion.div>
+                </div>
 
-                <motion.h1
-                  initial="hidden"
-                  animate="show"
-                  variants={titleVariants}
-                  className={s.gradientText}
-                >
-                  Jackson Main
-                </motion.h1>
+                <h1 className={s.gradientText}>Jackson Main</h1>
 
                 <div className="flex mt-3 items-center text-gray-400 justify-center lg:justify-start">
                   {/* Profile Pic large screen */}
-                  <motion.div
-                    initial="hidden"
-                    animate="show"
-                    variants={subTitleVariants}
-                    className="lg:block hidden"
-                  >
-                    <img
-                      className="h-12 w-12 rounded-full object-cover object-center shadow-md"
-                      src="/headshot.JPG"
-                      alt="picture of me"
-                    />
-                  </motion.div>
+                  <div className="lg:block hidden">
+                    <img className={s.subheadingImg} src="/headshot.JPG" alt="picture of me" />
+                  </div>
 
-                  <motion.span
-                    initial="hidden"
-                    animate="show"
-                    variants={subTitleVariants}
-                    className="block lg:ml-3 text-sm md:text-base lg:text-lg lg:text-gray-400"
-                  >
+                  <span className={s.subheading}>
                     Full Stack Developer | Aspiring UX/UI Designer
-                  </motion.span>
+                  </span>
                 </div>
-                <motion.h2
-                  initial="hidden"
-                  animate="show"
-                  variants={headlineVariants}
-                  className="sm:text-lg md:text-xl lg:text-2xl px-1 text-gray-300 tracking-wide mt-10 max-w-2xl"
-                >
+                <h2 className={s.headline}>
                   Striving to provide the most memorable and meaningful user experiences on the
                   web.
-                </motion.h2>
+                </h2>
                 <div className="lg:flex lg:items-center">
-                  <motion.button
+                  <button
                     aria-label="scroll to my featured project section"
-                    initial="hidden"
-                    animate="show"
-                    variants={callToActionBtnVariants}
                     className={s.callToAction}
                     onClick={() => {
                       scrollToElement("ftProjSection");
                     }}
                   >
                     Let's see my work!
-                  </motion.button>
-                  <motion.div
-                    initial="hidden"
-                    animate="show"
-                    variants={arrowDownVariants}
-                    className="flex justify-center mt-20 animate-bounce lg:ml-20"
-                  >
+                  </button>
+                  <div className="flex justify-center mt-20 animate-bounce lg:ml-20">
                     <ArrowDown
                       onClick={() => {
                         scrollToElement("ftProjSection");
                       }}
-                      className="h-10 w-10 fill-current text-gray-50 cursor-pointer"
+                      className={s.arrowDown}
                     />
-                  </motion.div>
+                  </div>
                 </div>
               </div>
 
