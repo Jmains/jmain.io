ac61787e88cc32c3ce01283a434e1bebf95ccc62
Jackson <jacksonmain9@gmail.com>
2021-04-20 18:48:52 -0500

    added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,22 @@
 import { AppProps } from "next/dist/next-server/lib/router/router";
 import "./globals.css";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import Layout from "@components/shared/Layout";
 import { UIProvider } from "../components/ui/context";
+import * as gtag from "@utils/gtag";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <UIProvider>
       <Layout>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { GA_TRACKING_ID } from "../utils/gtag";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: any) {
@@ -14,6 +15,23 @@ class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700;800&display=swap"
             rel="stylesheet"
+          />
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
           />
           {/* <script
             src="https://www.google.com/recaptcha/api.js?&render=explicit"
