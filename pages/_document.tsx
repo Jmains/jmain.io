f4ef37ddaa6f83859917aa9afa724bb01091cce4
Jackson <jacksonmain9@gmail.com>
2021-04-27 16:50:04 -0500

    changed favicon for all browsers

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,10 @@ class MyDocument extends Document {
     return (
       <Html lang="eng">
         <Head>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700;800&display=swap"
